aedda189fc04dd2be78992af05781d4e4b932daa
JRS050 <rampinojonathan@gmail.com>
2025-07-23 15:44:25 +0200

    Initial upload and setup

---
+++ app/Models/Book.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Book extends Model
+{
+    public function author() {
+        return $this->belongsTo(Author::class);
+    }
+    public function reviews() {
+        return $this->hasMany(Review::class);
+    }
+    use HasFactory;
+}
