f098748c1d0f43ae7846ad61ec082ec55d2374b4
JRS050 <rampinojonathan@gmail.com>
2025-08-20 16:00:59 +0200

    Review edit

--- resources/js/domains/reviews/routes.ts
+++ resources/js/domains/reviews/routes.ts
@@ -2,6 +2,6 @@ import BookReviews from "./components/BookReviews.vue";
 import Edit from "./pages/Edit.vue";
 
 export const reviewRoutes = [
-    {path: '/reviews', Component: BookReviews, name: 'reviews.show'},
-    {path: '/reviews/:id/edit', component: Edit, name: 'reviews.edit'},
+    {path: '/reviews', component: BookReviews, name: 'reviews.show'},
+    {path: '/reviews/:id/edit', component: Edit, name: 'reviews.edit'}
 ];
